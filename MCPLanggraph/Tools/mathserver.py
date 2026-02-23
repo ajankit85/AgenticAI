9c69e9dd7b0f02227423bdcf2773d2216d6be018
Ankit Jain <aj.ankit85@gmail.com>
2026-02-22 18:32:05 -0600

    added more math operations

--- MCPLanggraph/Tools/mathserver.py
+++ MCPLanggraph/Tools/mathserver.py
@@ -14,6 +14,18 @@ def multiple(a:int,b:int)-> int:
     """Multiply two numbers"""
     return a * b
 
+
+@mcp.tool()
+def subtract(a:int,b:int)-> int:
+    """Subtract two numbers"""
+    return a - b
+
+@mcp.tool()
+def divide(a:int,b:int)-> int:
+    """Divide two numbers"""
+    return a // b if b != 0 else 0
+
+
 #The transport="stdio" argument tells the server to:
 #Use standard input/output (stdin and stdout) to receive and respond to tool function calls.
 
